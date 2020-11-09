8ef805f347e92dcde62fcaea068565c37123c7ea
wei_chen <wei_chen@mail.chungyo.net>
2020-11-09 12:08:04 +0800

    調整UI讓顯示更直覺,新增readme.md內容

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 圖片轉文字符號
+
+## 顯示單/雙線程函式執行時間
+
+首頁
+![image](gitImage/index.png)
+效果 1 終端機圖示
+![image](gitImage/cmd.png)
+效果 2 貓 （textarea 可滾動，查看全圖）
+![image](gitImage/cat.png)
--- backend.go
+++ backend.go
@@ -15,7 +15,9 @@ import (
     "github.com/nfnt/resize"
     "strconv"
     "time"
+    "encoding/base64"
 )
+//特殊import 只執行庫的init() 不使用其他方法
 import _ "image/jpeg"
 import _ "image/png"
 
@@ -26,24 +28,17 @@ type IndexData struct {
     Error string
     Result string
     GoroutineResult string
+    //前端圖片格式
+    ImageHeight string
+    ImageWidth string
+    ImageBase64 string
 }
+
 const pageTitle = "將上傳圖片轉成文字符號"
 const pageContent ="寬度若大於300像素，會等比壓縮至300像素"
-var symbol = [16]string{" ",".","-","^","~","!","(",")","=","%","&","$","/","*","#","@" } 
+var symbol = [16]string{" ",".","-","^","~","!","(",")","=","%","&","$","+","*","#","@" } 
     
-func indexPage(w http.ResponseWriter, r *http.Request)  {
-	tmpl := template.Must(template.ParseFiles("./index.html"))
-	data:=new(IndexData)
-	data.Title = pageTitle
-	data.Content =pageContent
-	tmpl.Execute(w,data)
-}
-func frontJS(w http.ResponseWriter, r *http.Request)  {
-	tmpl := template.Must(template.ParseFiles("./front.js"))
-	
-	tmpl.Execute(w,"null")
-}
-// 上传图像接口
+// 上傳圖片
 func uploadHandle (w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Content-Type", "text/html")
  
@@ -55,7 +50,7 @@ func uploadHandle (w http.ResponseWriter, req *http.Request) {
 	    data.Content =pageContent
 	    tmpl.Execute(w,data)
     } else {
-        // 接收图片
+        // 接收圖片
         uploadFile, handle, err := req.FormFile("image")
         fmt.Println(err)
         if err != nil{
@@ -63,17 +58,16 @@ func uploadHandle (w http.ResponseWriter, req *http.Request) {
             return
         }
         
-        // 检查图片后缀
+        // 檢查圖片副檔名
         ext := strings.ToLower(path.Ext(handle.Filename))
         if ext != ".jpg" && ext != ".png" {
             errorHandle(errors.New("只支援jpg/png檔案格式"), w);
             return
-            //defer os.Exit(2)
         }
         
 		saveFileName := []byte(handle.Filename)
         fmt.Println(string(saveFileName))
-        // 保存图片
+        // 保存圖片
         os.Mkdir("./uploaded/", 0777)
         saveFile, err := os.OpenFile("./uploaded/" + string(saveFileName) , os.O_WRONLY|os.O_CREATE, 0666);
         errorHandle(err, w)
@@ -81,27 +75,33 @@ func uploadHandle (w http.ResponseWriter, req *http.Request) {
  
         defer uploadFile.Close()
         defer saveFile.Close()
-        // 上传图片成功
+        // 上傳圖片
         tmpl := template.Must(template.ParseFiles("./uploaded.html"))
 	    data:=new(IndexData)
 	    data.Title = pageTitle
         data.Content =pageContent
         data.Filename =handle.Filename
-        // w.Write([]byte("查看上傳圖片: <a target='_blank' href='/uploaded/" + handle.Filename + "'>" + handle.Filename + "</a>"));
-        data.Result = imageToSymbol("./uploaded/"+string(saveFileName),w)
+        f,_:=ioutil.ReadFile("./uploaded/"+string(saveFileName))
+       
+        
+        data.ImageBase64 = base64.StdEncoding.EncodeToString(f)
+        
+        data.Result,data.ImageWidth,data.ImageHeight = imageToSymbol("./uploaded/"+string(saveFileName),w)
         data.GoroutineResult = imageToSymbol2("./uploaded/"+string(saveFileName),w)
         
         tmpl.Execute(w,data)
     }
 }
-func imageToSymbol(name string, web http.ResponseWriter)  string{
+//圖片轉符號（單線程）
+func imageToSymbol(name string, web http.ResponseWriter)  (string ,string,string){
     t1:=time.Now()
     fmt.Println(name)
     file, err := os.Open(name)
     errorHandle(err, web);
     defer file.Close()
     imageDecode, _, err := image.Decode(file)
-    if imageDecode.Bounds().Dx()>300 {
+    //重設圖片大小
+    if imageDecode.Bounds().Dx()>300 || imageDecode.Bounds().Dy()>300 {
         imageDecode = resize.Resize(300,0,imageDecode,resize.Lanczos3)
     }
     
@@ -117,19 +117,21 @@ func imageToSymbol(name string, web http.ResponseWriter)  string{
             _,g,_,_:=colorRgb.RGBA()
             g_uint8 := uint8(g >>8)
             sum := (255-g_uint8)/16
+            //取用符號陣列
             row +=symbol[sum]
         }
         row += "\n"
         // fmt.Println(" ")
         
     }
-    web.Write([]byte("圖片寬："+strconv.Itoa(dx)+" 圖片高："+strconv.Itoa(dy)+"<br>"))
+    
     fmt.Println(dx,dy)
     elapsed := time.Since(t1)
     fmt.Println("單線程時間:" , elapsed)
-    web.Write([]byte("單線程時間：" + elapsed.String()+"<br>"))
-    return row
+    web.Write([]byte("<p style='border-style:solid;border-color:blue;'>單線程時間：" + elapsed.String()+"</p>"))
+    return row,strconv.Itoa(dx),strconv.Itoa(dy)
 }
+//圖片轉符號（兩線程）
 func imageToSymbol2(name string, web http.ResponseWriter)  string{
 
     t2:=time.Now()
@@ -138,7 +140,7 @@ func imageToSymbol2(name string, web http.ResponseWriter)  string{
     errorHandle(err, web);
     defer file.Close()
     imageDecode, _, err := image.Decode(file)
-    if imageDecode.Bounds().Dx()>300 {
+    if imageDecode.Bounds().Dx()>300 || imageDecode.Bounds().Dy()>300{
         imageDecode = resize.Resize(300,0,imageDecode,resize.Lanczos3)
     }
     errorHandle(err, web);
@@ -182,11 +184,11 @@ func imageToSymbol2(name string, web http.ResponseWriter)  string{
     // fmt.Println(result)
     fmt.Println(dx,dy)
     elapsed := time.Since(t2)
-    fmt.Println("多線程時間:" , elapsed)
-    web.Write([]byte("多線程時間：" + elapsed.String()))
+    fmt.Println("雙線程時間:" , elapsed)
+    web.Write([]byte("<p style='border-style:solid;border-color:red;'>雙線程時間：" + elapsed.String()+"</p>"))
     return result
 }
-// 显示图片接口
+// 顯示原始圖片
 func showPicHandle( w http.ResponseWriter, req *http.Request ) {
     file, err := os.Open("." + req.URL.Path)
     errorHandle(err, w);
@@ -197,7 +199,7 @@ func showPicHandle( w http.ResponseWriter, req *http.Request ) {
     w.Write(buff)
 }
  
-// 统一错误输出接口
+// 統一錯誤輸出
 func errorHandle(err error, w http.ResponseWriter) {
     if  err != nil {
         tmpl := template.Must(template.ParseFiles("./index.html"))
@@ -211,9 +213,6 @@ func errorHandle(err error, w http.ResponseWriter) {
     }
 }
 func main()  {
-	// http.HandleFunc("/",indexPage)
-	// http.HandleFunc("/index",indexPage)
-	// http.HandleFunc("/front.js",frontJS)
 	http.HandleFunc("/upload/",uploadHandle)
 	http.HandleFunc("/uploaded/",showPicHandle)
 
